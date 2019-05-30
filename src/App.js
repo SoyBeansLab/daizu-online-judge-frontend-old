a3caefc606b473ad7dffb5d0a718e47555cab5ba
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-05-30 14:35:58 +0900

    feat: eslintとprettier導入, ディレクトリ構成も変更した

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
   return (
--- src/materialui/theme.js
+++ src/materialui/theme.js
@@ -1,23 +1,21 @@
-import {
-  createMuiTheme
-} from '@material-ui/core/styles'
+import { createMuiTheme } from "@material-ui/core/styles";
 
 export const theme = createMuiTheme({
   palette: {
     /* メインカラー */
     primary: {
-      light: '#4ebaaa',
-      main: '#00897b',
-      dark: '#005b4f',
-      contrastText: '#212121' /* テキストカラー(黒) */
+      light: "#4ebaaa",
+      main: "#00897b",
+      dark: "#005b4f",
+      contrastText: "#212121" /* テキストカラー(黒) */
     },
     /* アクセントカラー */
     secondary: {
-      light: '#ff77a9',
-      main: '#ec407a',
-      dark: '#b4004e',
-      contrastText: '#f5f5f5' /* テキストカラー(白) */
+      light: "#ff77a9",
+      main: "#ec407a",
+      dark: "#b4004e",
+      contrastText: "#f5f5f5" /* テキストカラー(白) */
     }
   }
-})
+});
 export default theme;
