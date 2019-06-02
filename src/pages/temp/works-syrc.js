d1a7ec6f89478190898b05470816c12a7ea2db51
mosmosyrc <prfm_love2.mosmos@icloud.com>
2019-06-02 23:35:00 +0900

    feat:AppBarを追加

--- src/pages/temp/works-syrc.js
+++ src/pages/temp/works-syrc.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+function SimpleAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6">Photos</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default SimpleAppBar;
+
+/*import React from "react";
 
 function Works() {
   return (
     <div className="works">
-      <h2>hello mosmos_syrc!</h2>
+      <h2>helloWorld!</h2>
     </div>
   );
 }
-export default Works;
+export default Works;*/
