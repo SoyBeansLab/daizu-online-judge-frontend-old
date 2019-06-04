cfc88b80bd473333b9f866f5ea6f170b5624cf7f
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-06-04 18:11:29 +0900

    feat: linkの色をmain.cssで定義

--- src/pages/temp/works-rishi.js
+++ src/pages/temp/works-rishi.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import { Route, Link } from "react-router-dom";
+// import Typo from "@material-ui/core/Typography";
+import test from "./works-syrc";
 
 function Works() {
   return (
     <div className="works">
-      <h2>hello rishi!</h2>
+      <Link to="/test" className="link">
+        wa
+      </Link>
+      <Route to="/test" component={test} />
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
     </div>
   );
 }
