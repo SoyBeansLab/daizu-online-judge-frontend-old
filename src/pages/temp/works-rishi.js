139e331088d118c97cf749b892bff543a5e96ec0
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-06-20 17:45:56 +0900

    feat: 背景なしのテキストのみのボタンを実装

--- src/pages/temp/works-rishi.js
+++ src/pages/temp/works-rishi.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NormalButton from "../../atoms/app-button-normal";
+import TextButton from "../../atoms/app-button-text";
 
 const Works = () => (
   <div>
     <h1>Hello</h1>
     <NormalButton buttonText="huga" />
+    <TextButton buttonText="hoge" />
   </div>
 );
 
