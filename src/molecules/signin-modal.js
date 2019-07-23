b712ac37310adaa08c2c18a384d97fcf6d951aa3
mosmosyrc <prfm_love2.mosmos@icloud.com>
2019-07-23 18:37:35 +0900

    feat: Signin/upのModalのレイアウト一旦完成。

---
+++ src/molecules/signin-modal.js
@@ -0,0 +1,202 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import PropTypes from "prop-types";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    minWidth: 450,
+  },
+  card: {
+    minWidth: 450,
+    maxWidth: 500,
+    margin: theme.spacing(1),
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    minWidth: 450,
+  },
+  checkbox: {
+    marginLeft: theme.spacing(1),
+  },
+  tabs: {
+    margin: theme.spacing(3),
+  },
+}));
+
+function SignInModal() {
+  const classes = useStyles();
+  const [state, setValues] = React.useState({
+    id: "",
+    password: "",
+    checkedA: false,
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...state, [name]: event.target.value });
+  };
+
+  return (
+    <div>
+      <CardActions>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="standard-id"
+            label="Your ID"
+            className={classes.textField}
+            value={state.id}
+            onChange={handleChange("id")}
+            margin="normal"
+            required="true"
+          />
+        </form>
+      </CardActions>
+      <CardActions>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="standard-password"
+            label="Password"
+            className={classes.textField}
+            value={state.password}
+            onChange={handleChange("password")}
+            margin="normal"
+            required="true"
+            type="password"
+          />
+        </form>
+      </CardActions>
+      <CardActions>
+        <FormControlLabel
+          control={
+            <Checkbox checked={state.checked} onChange={handleChange("checkedA")} value="checkedA" color="secondary" />
+          }
+          label="IDとパスワードを記憶する"
+          className={classes.checkbox}
+        />
+      </CardActions>
+      <CardActions>
+        <Button variant="contained" className={classes.button} color="secondary">
+          SIGN IN
+        </Button>
+      </CardActions>
+    </div>
+  );
+}
+
+function SignUpModal() {
+  const classes = useStyles();
+  const [state, setValues] = React.useState({
+    id: "",
+    email: "",
+    password: "",
+    checkedA: false,
+  });
+
+  const handleChange = name => event => {
+    setValues({ ...state, [name]: event.target.value });
+  };
+
+  return (
+    <div>
+      <CardActions>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="standard-id"
+            label="Your New ID"
+            className={classes.textField}
+            value={state.id}
+            onChange={handleChange("id")}
+            margin="normal"
+            required="true"
+          />
+        </form>
+      </CardActions>
+      <CardActions>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="standard-email"
+            label="E-Mail"
+            className={classes.textField}
+            value={state.email}
+            onChange={handleChange("email")}
+            margin="normal"
+            required="true"
+            type="email"
+          />
+        </form>
+      </CardActions>
+      <CardActions>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            id="standard-password"
+            label="Password"
+            className={classes.textField}
+            value={state.password}
+            onChange={handleChange("password")}
+            margin="normal"
+            required="true"
+            type="password"
+          />
+        </form>
+      </CardActions>
+      <CardActions>
+        <Button variant="contained" className={classes.button} color="secondary">
+          SIGN UP
+        </Button>
+      </CardActions>
+    </div>
+  );
+}
+
+// 仮データを突っ込むための関数
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+// propsの型指定
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default function ModalTab() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+  return (
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <Tabs className={classes.tabs} value={value} onChange={handleChange} indicatorColor="primary" centered={true}>
+          <Tab label="Sign in" />
+          <Tab label="Sign up" />
+        </Tabs>
+
+        {value === 0 && <SignInModal />}
+        {value === 1 && <SignUpModal />}
+      </Card>
+    </div>
+  );
+}
