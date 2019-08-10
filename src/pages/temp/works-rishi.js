0bd62ece113f2d9f59b05f263c2b2862afa3b49a
10riridk0 <mi151316@edu.okinawa-ct.ac.jp>
2019-08-11 01:52:53 +0900

    feat: contestsはえた

--- src/pages/temp/works-rishi.js
+++ src/pages/temp/works-rishi.js
@@ -1,17 +1,9 @@
 import React from "react";
-import Slider from "../../molecules/Slider";
-import ContestsListTabs from "../../molecules/ContestsListTabs";
-import ContestTabs from "../../molecules/ContestTabs";
+import Contests from "../../templates/Contests";
 
 const Works = () => (
   <div>
-    <h1>rishi works</h1>
-    <h2>Slider</h2>
-    <Slider />
-    <h2>Contests</h2>
-    <ContestsListTabs />
-    <h2>Contest</h2>
-    <ContestTabs />
+    <Contests />
   </div>
 );
 
