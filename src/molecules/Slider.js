5b9ec1e35165aa4ffa72961aec34776e73feb3fd
ucpr <hoge.uchihara@gmail.com>
2019-08-20 02:33:51 +0900

    chore: 必要のないコメントアウト部分を消した

--- src/molecules/Slider.js
+++ src/molecules/Slider.js
@@ -34,39 +34,3 @@ export default function HomeSlider(props) {
 HomeSlider.propTypes = {
   slideItemList: PropTypes.array,
 };
-/*
-class HomeSlider extends React.Component {
-  render() {
-    return (
-      <Slider {...settings}>
-        <div>
-          <SliderItem
-            contestTitle="contestTitle-1"
-            contestDate="2019.11.11"
-            contestTime="16:00-17:00"
-            contestContent="これはテストです"
-          />
-        </div>
-        <div>
-          <SliderItem
-            contestTitle="contestTitle-2"
-            contestDate="2019.12.12"
-            contestTime="16:00-17:00"
-            contestContent="これはテストです"
-          />
-        </div>
-        <div>
-          <SliderItem
-            contestTitle="contestTitle-3"
-            contestDate="2019.01.01"
-            contestTime="16:00-19:00"
-            contestContent="text is text"
-          />
-        </div>
-      </Slider>
-    );
-  }
-}
-
-export default HomeSlider;
-*/
