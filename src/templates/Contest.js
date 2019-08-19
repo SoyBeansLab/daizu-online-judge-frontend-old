b4f96ea27d17d36b1117459619580bda50e84059
ucpr <hoge.uchihara@gmail.com>
2019-08-20 01:59:50 +0900

    feat: propsからデータを取るようにした

--- src/templates/Contest.js
+++ src/templates/Contest.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import ContestTabs from "../molecules/ContestTabs";
 
@@ -14,11 +15,29 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function Contests() {
+export default function Contests(props) {
   const classes = useStyles();
+
+  const contestTopContent = props.contestTopContent;
+  const problemLists = props.problemLists;
+  const submitStatusLists = props.submitStatusLists;
+  const rankingLists = props.rankingLists;
+
   return (
     <div className={classes.root}>
-      <ContestTabs />
+      <ContestTabs
+        contestTopContent={contestTopContent}
+        problemLists={problemLists}
+        submitStatusLists={submitStatusLists}
+        rankingLists={rankingLists}
+      />
     </div>
   );
 }
+
+Contests.propTypes = {
+  contestTopContent: PropTypes.string,
+  problemLists: PropTypes.array,
+  submitStatusLists: PropTypes.array,
+  rankingLists: PropTypes.array,
+};
--- src/templates/Contests.js
+++ src/templates/Contests.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import ContestsListTabs from "../molecules/ContestsListTabs";
 import { makeStyles } from "@material-ui/core/styles";
 import Typograhy from "@material-ui/core/Typography";
@@ -15,14 +16,29 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function Contests() {
+export default function Contests(props) {
   const classes = useStyles();
+
+  const holdingContestLists = props.holdingContestLists;
+  const scheduledContestLists = props.scheduledContestLists;
+  const finishedContestLists = props.finishedContestLists;
+
   return (
     <div className={classes.root}>
       <Typograhy variant="h5" className={classes.heading}>
         コンテスト一覧
       </Typograhy>
-      <ContestsListTabs />
+      <ContestsListTabs
+        holdingContestLists={holdingContestLists}
+        scheduledContestLists={scheduledContestLists}
+        finishedContestLists={finishedContestLists}
+      />
     </div>
   );
 }
+
+Contests.propTypes = {
+  holdingContestLists: PropTypes.array,
+  scheduledContestLists: PropTypes.array,
+  finishedContestLists: PropTypes.array,
+};
