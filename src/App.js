5a04d2c568d1d3a40bb813c3aa126db4d033c20f
ucpr <hoge.uchihara@gmail.com>
2019-11-13 00:48:46 +0900

    feat: 存在しないpageに飛ぶと準備中のページに遷移するようにした

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./atoms/app-header";
 import Footer from "./atoms/app-footer";
 import Home from "./pages/Home";
 import Contests from "./pages/Contests";
 import Signin from "./templates/SignInModal";
-//import Preparation from "./templates/Preparation";
+import Preparation from "./templates/Preparation";
 import Debug from "./pages/temp/works-para";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -26,10 +26,13 @@ function App() {
       <BrowserRouter>
         <Header />
         <div className={classes.root}>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/contests" component={Contests} />
-          <Route exact path="/signin" component={Signin} />
-          <Route exact path="/debug" component={Debug} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/contests" component={Contests} />
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/debug" component={Debug} />
+            <Route component={Preparation} />
+          </Switch>
         </div>
       </BrowserRouter>
       <Footer />
