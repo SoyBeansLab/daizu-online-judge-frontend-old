ecddd8382e822e1dd79ebe71cd087580845b3a88
ucpr <hoge.uchihara@gmail.com>
2019-11-14 11:52:02 +0900

    chore: debug

--- src/pages/temp/works-para.js
+++ src/pages/temp/works-para.js
@@ -1,13 +1,11 @@
 import React from "react";
 //import UpModal from "../../molecules/SignUpModal.js";
 //import InModal from "../../molecules/SignInModal.js";
-import InModalTemplate from "../../templates/SignInModal";
-import UpModalTemplate from "../../templates/SignUpModal";
+import Contest from "../../templates/Contest";
 
 const Works = () => (
   <div>
-    <InModalTemplate />
-    <UpModalTemplate />
+    <Contest />
   </div>
 );
 
