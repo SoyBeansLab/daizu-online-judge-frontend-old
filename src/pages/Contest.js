e56e138e6f18a804f123a6ed62507c14adb7a385
ucpr <hoge.uchihara@gmail.com>
2019-11-28 14:33:52 +0900

    feat: requestを投げる処理を別ファイルに分けて、それぞれのページに適用した

--- src/pages/Contest.js
+++ src/pages/Contest.js
@@ -2,41 +2,19 @@ import React, { useEffect, useReducer } from "react";
 import ContestTemplate from "../templates/Contest";
 import PropTypes from "prop-types";
 import { reducer } from "../reducer";
-import axios from "axios";
+//import axios from "axios";
 import { request } from "../requests";
-import MockAdapter from "axios-mock-adapter";
-
-var mock = new MockAdapter(axios);
-
-mock.onGet("/contests/mitohato").reply(200, {
-  contest_top_content: "## Hello World!",
-  problem_list: [
-    {
-      problem_id: "hello_world",
-      problem_order: "A",
-      problem_name: "Hello World",
-      time_limit: 2,
-      memory_limit: 256,
-      problem_score: 100
-    },
-    {
-      problem_id: "hogehoge",
-      problem_order: "B",
-      problem_name: "hogehoge",
-      time_limit: 2,
-      memory_limit: 256,
-      problem_score: 100
-    }
-  ]
-});
+import urljoin from "url-join";
+//import { config } from "../config";
 
 export default function Contest(props) {
   const [state, dispatch] = useReducer(reducer, { loading: true, data: [] });
   const contestId = props.match.params.contest_id; // url paramから取得
+  const endpoint = urljoin("/contests", contestId);
 
   useEffect(() => {
-    request("/contests/" + contestId, dispatch);
-  }, [contestId]);
+    request(endpoint, dispatch);
+  }, [endpoint]);
 
   return (
     <ContestTemplate
--- src/pages/Contests.js
+++ src/pages/Contests.js
@@ -1,74 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import ContestsTemplate from "../templates/Contests.js";
-//import request from "../requests/requests.js";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
+import { reducer } from "../reducer";
+import { request } from "../requests";
 
-var mock = new MockAdapter(axios);
+export default function Contests() {
+  const [state, dispatch] = useReducer(reducer, {
+    loading: true,
+    data: []
+  });
+  const endpoint = "/contests";
 
-mock.onGet("/contests").reply(200, {
-  upcoming: [
-    // 予定されているやつ
-    {
-      contest_id: "mitohato",
-      contest_name: "mito_contest",
-      contest_date: "2019-08-17 12:00:00",
-      contest_time: 120,
-      writer: "mitohato",
-      contest_description: "Hello World!",
-      problem_number: 2
-    },
-    {
-      contest_id: "hogehoge",
-      contest_name: "hoge_contest",
-      contest_date: "2019-08-17 12:00:00",
-      contest_time: 120,
-      writer: "mitohato",
-      contest_description: "Hello World!",
-      problem_number: 2
+  const switchComponent = key => {
+    if (state.loading) {
+      return [];
+    } else {
+      return state.data[key];
     }
-  ],
-  current: [
-    // 開催中のやつ
-    {
-      contest_id: "mitohato",
-      contest_name: "mito_contest",
-      contest_date: "2019-08-17 12:00:00",
-      contest_time: 120,
-      writer: "mitohato",
-      contest_description: "",
-      problem_number: 2
-    }
-  ],
-  recent: [] // 終了したやつ
-});
+  };
 
-export default function Home() {
-  const [state, setState] = useState({ isDone: false, slideItems: [] });
   useEffect(() => {
-    axios
-      .get("contests")
-      .then(function(response) {
-        const data = { ...response.data };
-        setState({ isDone: true, slideItems: data });
-      })
-      .catch(function(error) {
-        console.log(error); //eslint-disable-line
-      })
-      .finally(function() {});
-  }, []);
+    request(endpoint, dispatch);
+  }, [endpoint]);
 
   return (
     <ContestsTemplate
-      currentContestLists={
-        state.isDone ? Object.values(state.slideItems.current) : []
-      }
-      upcomingContestLists={
-        state.isDone ? Object.values(state.slideItems.upcoming) : []
-      }
-      recentContestLists={
-        state.isDone ? Object.values(state.slideItems.recent) : []
-      }
+      currentContestLists={Object.values(switchComponent("current"))}
+      upcomingContestLists={Object.values(switchComponent("upcoming"))}
+      recentContestLists={Object.values(switchComponent("recent"))}
     />
   );
 }
