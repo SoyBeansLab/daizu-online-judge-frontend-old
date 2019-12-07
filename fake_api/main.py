c8924fbcddec98491838f0a2f1f33d3092aee4d3
ucpr <hoge.uchihara@gmail.com>
2019-12-07 15:03:14 +0900

    test: add test datas

--- fake_api/main.py
+++ fake_api/main.py
@@ -137,6 +137,69 @@ def lang():
     result = ["C", "C++", "Python", "Java"]
     return jsonify(result)
 
- 
+@app.route("/contests/<contest_id>/submits", methods=["GET"])
+def submit_list(contest_id):
+    result = [
+        {
+            "submit_id": "123456",
+            "submit_date": "2019-05-05",
+            "problem_name": "Douteki keikaku plan",
+            "language": "C++",
+            "score": 100,
+            "code_length": 100,
+            "result": "AC",
+            "username": "nemu_sou",
+            "execute_time": 300,
+            "memory": 400,
+            "source_code": 'printf("HelloWorld");'
+        },
+        {
+            "submit_id": "929292",
+            "submit_date": "2019-05-05",
+            "problem_name": "Hello World",
+            "language": "C++",
+            "score": 100,
+            "code_length": 100,
+            "result": "WA",
+            "username": "nemu_sou",
+            "execute_time": 300,
+            "memory": 400,
+            "source_code": 'printf("HelloWorld");'
+        },
+        {
+            "submit_id": "122222",
+            "submit_date": "2019-05-05",
+            "problem_name": "Douteki keikaku plan",
+            "language": "C++",
+            "score": 100,
+            "code_length": 100,
+            "result": "WJ",
+            "username": "nemu_sou",
+            "execute_time": 300,
+            "memory": 400,
+            "source_code": 'printf("HelloWorld");'
+        },
+    ]
+    return jsonify(result)
+
+
+
+@app.route("/contests/<contest_id>/submits/<submit_id>", methods=["GET"])
+def submit(contest_id, submit_id):
+    result = {
+        "submit_id": "123456",
+        "submit_date": "2019-05-05",
+        "problem_name": "Douteki keikaku plan",
+        "language": "C++",
+        "score": 100,
+        "code_length": 100,
+        "result": "AC",
+        "username": "nemu_sou",
+        "execute_time": 300,
+        "memory": 400,
+        "source_code": 'printf("HelloWorld");'
+    }
+    return jsonify(result)
+
 if __name__ == "__main__":
     app.run()
