d20bc638da0a93ec5fc6972080335cd2aabcd9a7
ucpr <hoge.uchihara@gmail.com>
2019-12-09 13:12:15 +0900

    feat: 別のmethodとpayloadを渡せるようにした

--- src/requests.js
+++ src/requests.js
@@ -2,12 +2,20 @@ import axios from "axios";
 import urljoin from "url-join";
 import { config } from "./config";
 
-export const request = async (endpoint, dispatch) => {
+export const request = async (
+  endpoint,
+  dispatch,
+  method = "get",
+  data = {}
+) => {
   dispatch({ type: "REQUEST_SENDING" });
 
   const url = urljoin(config.DEV_API_SERVER, endpoint);
-  axios
-    .get(url)
+  axios({
+    method: method,
+    url: url,
+    data: data
+  })
     .then(function(response) {
       const data = { ...response.data };
       dispatch({ type: "REQUEST_SUCCESS", data: data });
