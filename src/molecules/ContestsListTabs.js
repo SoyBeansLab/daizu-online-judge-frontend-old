74938972d555a4bca43a0dc14bf994952c1a41fe
ucpr <hoge.uchihara@gmail.com>
2020-03-10 03:12:11 +0900

    feat: Contests Tabのpositionをquery paramsで管理するようにした

--- src/molecules/ContestsListTabs.js
+++ src/molecules/ContestsListTabs.js
@@ -7,6 +7,9 @@ import Tabs from "@material-ui/core/Tabs";
 // テーブル
 import ContestsTable from "./ContestsTable";
 
+import { Link, useLocation } from "react-router-dom";
+import { parse } from "query-string";
+
 const useStyles = makeStyles(() => ({
   root: {
     width: "100%",
@@ -21,8 +24,18 @@ const useStyles = makeStyles(() => ({
 export default function ContestsListTabs(props) {
   const tabValueList = ["current", "upcoming", "recent"];
 
+  const location = useLocation();
+  const getTabPostion = () => {
+    if (!location.search) {
+      return tabValueList[0];
+    }
+
+    const parsed = parse(location.search);
+    return parsed.tab;
+  };
+
   const classes = useStyles();
-  const [value, setValue] = React.useState(tabValueList[0]);
+  const [value, setValue] = React.useState(getTabPostion());
 
   const currentContestLists = props.currentContestLists;
   const upcomingContestLists = props.upcomingContestLists;
@@ -35,9 +48,9 @@ export default function ContestsListTabs(props) {
   return (
     <div className={classes.root}>
       <Tabs value={value} onChange={handleChange} indicatorColor="primary" className={classes.tabs}>
-        <Tab label="開催中" value={tabValueList[0]} />
-        <Tab label="開催予定" value={tabValueList[1]} />
-        <Tab label="終了" value={tabValueList[2]} />
+        <Tab label="開催中" value={tabValueList[0]} component={Link} to={"/contests?tab=" + tabValueList[0]} />
+        <Tab label="開催予定" value={tabValueList[1]} component={Link} to={"/contests?tab=" + tabValueList[1]} />
+        <Tab label="終了" value={tabValueList[2]} component={Link} to={"/contests?tab=" + tabValueList[2]} />
       </Tabs>
       {value === tabValueList[0] && <ContestsTable contestLists={currentContestLists} />}
       {value === tabValueList[1] && <ContestsTable contestLists={upcomingContestLists} />}
