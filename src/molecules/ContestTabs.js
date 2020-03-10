623c86a2d3d9385b9227c6cafdcd56f6b4615c16
ucpr <hoge.uchihara@gmail.com>
2020-03-10 22:59:26 +0900

    feat: ContestのTabのpositionをqueryで管理するようにした

--- src/molecules/ContestTabs.js
+++ src/molecules/ContestTabs.js
@@ -11,6 +11,9 @@ import RankingTable from "./RankingTable";
 // コンテストトップ
 import TopContents from "./ContestTopContents";
 
+import { Link, useLocation } from "react-router-dom";
+import { parse } from "query-string";
+
 const useStyles = makeStyles(() => ({
   root: {
     width: "100%",
@@ -23,8 +26,20 @@ const useStyles = makeStyles(() => ({
 }));
 
 export default function ContestsListTabs(props) {
+  const tabValueList = ["top", "problems", "submits", "ranking"];
+
+  const location = useLocation();
+  const getTabPostion = () => {
+    if (!location.search) {
+      return tabValueList[0];
+    }
+
+    const parsed = parse(location.search);
+    return parsed.tab;
+  };
+
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(getTabPostion());
 
   const contestTopContent = props.contestTopContent;
   const problemLists = props.problemLists;
@@ -35,18 +50,39 @@ export default function ContestsListTabs(props) {
   function handleChange(event, newValue) {
     setValue(newValue);
   }
+
   return (
     <div className={classes.root}>
       <Tabs value={value} onChange={handleChange} indicatorColor="primary" className={classes.tabs}>
-        <Tab label="トップ" />
-        <Tab label="問題一覧" />
-        <Tab label="提出状況" />
-        <Tab label="ランキング" />
+        <Tab
+          label="トップ"
+          value={tabValueList[0]}
+          component={Link}
+          to={location.pathname + `?tab=${tabValueList[0]}`}
+        />
+        <Tab
+          label="問題一覧"
+          value={tabValueList[1]}
+          component={Link}
+          to={location.pathname + `?tab=${tabValueList[1]}`}
+        />
+        <Tab
+          label="提出状況"
+          value={tabValueList[2]}
+          component={Link}
+          to={location.pathname + `?tab=${tabValueList[2]}`}
+        />
+        <Tab
+          label="ランキング"
+          value={tabValueList[3]}
+          component={Link}
+          to={location.pathname + `?tab=${tabValueList[3]}`}
+        />
       </Tabs>
-      {value === 0 && <TopContents contestTopContent={contestTopContent} />}
-      {value === 1 && <ProblemsTable problemLists={problemLists} contestId={contestId} />}
-      {value === 2 && <SubmitStatusTable contestId={contestId} />}
-      {value === 3 && <RankingTable contestId={contestId} />}
+      {value === tabValueList[0] && <TopContents contestTopContent={contestTopContent} />}
+      {value === tabValueList[1] && <ProblemsTable problemLists={problemLists} contestId={contestId} />}
+      {value === tabValueList[2] && <SubmitStatusTable contestId={contestId} />}
+      {value === tabValueList[3] && <RankingTable contestId={contestId} />}
     </div>
   );
 }
