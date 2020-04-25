c9b7986b841533ff7b475bbc106319b38782add3
ucpr <contact@ucpr.dev>
2020-04-25 23:48:29 +0900

    feat: contestsのactionsとtypesを追加

---
+++ src/state/ducks/contests/actions.js
@@ -0,0 +1,15 @@
+import types from "./types";
+
+export const fetchContests = filter => ({
+  type: types.FETCH_CONTESTS,
+  filter,
+});
+
+export const fetching = () => ({
+  types: types.LOADING,
+});
+
+export const receiveContests = json => ({
+  types: types.FETCH_CONTESTS_SUCCESS,
+  data: json,
+});
