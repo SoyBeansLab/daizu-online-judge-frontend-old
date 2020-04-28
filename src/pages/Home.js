150e8058a63aab680308d623bb2ec15f62ca213e
ucpr <contact@ucpr.dev>
2020-04-29 01:36:51 +0900

    feat: reduxに接続するようにした

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,33 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import HomeTemplate from "../templates/Home.js";
-import { reducer } from "../reducer";
-import { request } from "../requests";
+import { contestsOperations } from "../state/ducks/contests";
 
-export default function Home() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: true,
-    data: [],
-  });
+const HomeContainer = ({ slideItemList, fetch }) => {
   const endpoint = "/contests";
 
   useEffect(() => {
-    request(endpoint, dispatch);
-  }, [endpoint]);
+    fetch(endpoint);
+  }, [endpoint, fetch]);
 
-  return <HomeTemplate slideItemList={Object.values(!state.loading ? state.data.upcoming : [])} />;
-}
+  console.log(slideItemList);
+  return <HomeTemplate slideItemList={Object.values(slideItemList)} />;
+};
+
+HomeContainer.propTypes = {
+  slideItemList: PropTypes.object,
+  fetch: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+  slideItemList: state.contestsState.contests.data.upcoming || [],
+});
+
+const mapDispatchToProps = {
+  fetch: contestsOperations,
+};
+
+const Home = connect(mapStateToProps, mapDispatchToProps)(HomeContainer);
+
+export default Home;
