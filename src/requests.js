235620b42a7165f03a4201a2007a2e48f53ee0d5
ucpr <contact@ucpr.dev>
2020-04-29 01:28:59 +0900

    feat: axiosClientを消した

--- src/requests.js
+++ src/requests.js
@@ -26,5 +26,3 @@ export const request = async (endpoint, dispatch, method = "get", data = {}) => 
       console.log("GET " + url); //eslint-disable-line
     });
 };
-
-export const axiosClient = axios.create({ baseURL: config.DEV_API_SERVER });
