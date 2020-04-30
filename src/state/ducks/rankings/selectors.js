8ca2858879623f129745606481ebb2449f5774d2
ucpr <contact@ucpr.dev>
2020-05-01 05:41:15 +0900

    feat: APIでtotalも要素として受け取るようにした

--- src/state/ducks/rankings/selectors.js
+++ src/state/ducks/rankings/selectors.js
@@ -1,12 +1,15 @@
 import { createSelector } from "reselect";
 
-const rankingsSelector = state => state.rankingsState.rankings.data;
+const rankingTotalSelector = state => state.rankingsState.rankings.data.total;
+
+const rankingsSelector = state => state.rankingsState.rankings.data.data;
 
 const limitedRankingSelector = createSelector(rankingsSelector, rankings => rankings);
 
 const isfetched = createSelector(rankingsSelector, rankings => rankings.upcoming !== void 0);
 
 export default {
+  rankingTotalSelector,
   rankingsSelector,
   limitedRankingSelector,
   isfetched,
