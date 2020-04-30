b204ca19795708bd3a459f83aebde7ab8c69774d
ucpr <contact@ucpr.dev>
2020-04-30 10:40:54 +0900

    test: データの量を増やした

--- src/mocks/contests/_contest_id/ranking/index.js
+++ src/mocks/contests/_contest_id/ranking/index.js
@@ -23,6 +23,42 @@ const ranking = [
     total: "0/5",
     score: "0",
   },
+  {
+    rank: 5,
+    user_id: "mitohato",
+    total: "5/5",
+    score: "5000",
+  },
+  {
+    rank: 6,
+    user_id: "222mitohato",
+    total: "5/5",
+    score: "5000",
+  },
+  {
+    rank: 7,
+    user_id: "fdmitohato",
+    total: "2/5",
+    score: "1000",
+  },
+  {
+    rank: 8,
+    user_id: "0mitohato",
+    total: "0/5",
+    score: "0",
+  },
+  {
+    rank: 9,
+    user_id: "fdmitohato",
+    total: "2/5",
+    score: "1000",
+  },
+  {
+    rank: 10,
+    user_id: "0mitohato",
+    total: "0/5",
+    score: "0",
+  },
 ];
 
 module.exports = {
