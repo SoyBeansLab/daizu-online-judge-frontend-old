66b55d7aa7d040dcf4b9dd2731baae6ab312cd5c
ucpr <contact@ucpr.dev>
2020-05-11 23:18:59 +0900

    feat: 新しいデータのselectorを作った

--- src/state/ducks/languages/selectors.js
+++ src/state/ducks/languages/selectors.js
@@ -2,9 +2,14 @@ import { createSelector } from "reselect";
 
 const languagesSelector = state => state.languagesState.languages.data || [];
 
+const languageListSelector = createSelector(languagesSelector, languages => Object.keys(languages));
+
+const languageDictSelector = createSelector(languagesSelector, languages => languages);
+
 const isfetched = createSelector(languagesSelector, languages => languages[0] !== void 0);
 
 export default {
-  languagesSelector,
+  languageListSelector,
+  languageDictSelector,
   isfetched,
 };
