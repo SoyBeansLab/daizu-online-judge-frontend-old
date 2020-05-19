c576b3e2805df565a8bc0207251bc2fc47be03eb
ucpr <contact@ucpr.dev>
2020-05-19 22:44:55 +0900

    feat: redux stateにrouterを追加した

--- src/state/store.js
+++ src/state/store.js
@@ -1,11 +1,18 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import thunk from "redux-thunk";
+import { createBrowserHistory } from "history";
 import * as reducers from "./ducks";
 
 // https://github.com/jhen0409/react-native-debugger/issues/280
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+export const history = createBrowserHistory();
+
 export default function configureStore(initialState = {}) {
-  const rootReducer = combineReducers(reducers);
-  return createStore(rootReducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+  const rootReducer = combineReducers({
+    ...reducers,
+    router: connectRouter(history),
+  });
+  return createStore(rootReducer, initialState, composeEnhancer(applyMiddleware(thunk, routerMiddleware(history))));
 }
