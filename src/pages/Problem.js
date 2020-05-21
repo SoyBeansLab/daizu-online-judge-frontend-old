c271671acba85f799e0f1ec4ae9d3e14e2a9499e
ucpr <contact@ucpr.dev>
2020-05-21 22:22:15 +0900

    feat: reduxで状態を持つようにしたので不要な記述を消した

--- src/pages/Problem.js
+++ src/pages/Problem.js
@@ -1,37 +1,17 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { useParams } from "react-router-dom";
 import ProblemTemplate from "../templates/Problem";
 import PropTypes from "prop-types";
-import { reducer } from "../reducer";
-import { request } from "../requests";
 import { languagesOperations, languagesSelectors } from "../state/ducks/languages";
 
 const ProblemContainer = ({ languages, languageDict, isLanguagesFetched, fetchLanguages }) => {
-  const [state, dispatch] = useReducer(reducer, { loading: true, data: [] });
-  const { contestId, problemId } = useParams();
-  const endpoint = `/contests/${contestId}/problems/${problemId}`;
-
   useEffect(() => {
-    request(endpoint, dispatch);
     if (!isLanguagesFetched) {
       fetchLanguages("/languages");
     }
-  }, [fetchLanguages, isLanguagesFetched, endpoint]);
+  }, [fetchLanguages, isLanguagesFetched]);
 
-  console.log(languages);
-  return (
-    <ProblemTemplate
-      problemId={state.data.problem_id}
-      problemName={state.data.problem_name}
-      timeLimit={state.data.time_limit}
-      memoryLimit={state.data.memory_limit}
-      score={state.data.score}
-      problemText={state.data.problem_detail}
-      languageLists={Object.values(languages)}
-      languageDict={languageDict}
-    />
-  );
+  return <ProblemTemplate languageLists={Object.values(languages)} languageDict={languageDict} />;
 };
 
 ProblemContainer.propTypes = {
