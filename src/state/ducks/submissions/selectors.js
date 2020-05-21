dc1b45cb16423dcc6adb972a38d7f6335db11813
ucpr <contact@ucpr.dev>
2020-05-21 23:37:11 +0900

    feat: submissionsを正規化してから保存するようにした

--- src/state/ducks/submissions/selectors.js
+++ src/state/ducks/submissions/selectors.js
@@ -1,8 +1,8 @@
 import { createSelector } from "reselect";
 
-const submissionsTotalSelector = state => state.submissionsState.submissions.data.total || 10;
+const submissionsTotalSelector = state => state.submissionsState.submissions.data.result.total || 10;
 
-const submissionsSelector = state => state.submissionsState.submissions.data.data || [];
+const submissionsSelector = state => state.submissionsState.submissions.data.entities.submissions || {};
 
 const limitedSubmissionsSelector = createSelector(submissionsSelector, submissions => submissions);
 
@@ -10,10 +10,17 @@ const isfetched = createSelector(submissionsSelector, submissions => submissions
 
 const getSubmissionsPage = state => state.submissionsState.submissions.ui.page || 0;
 
+const getSubmissions = createSelector(submissionsSelector, submissions => Object.values(submissions));
+
+const getSubmissionById = (state, props) =>
+  state.submissionsState.submissions.data.entities.submissions[props.submitId] || {};
+
 export default {
   submissionsTotalSelector,
   submissionsSelector,
   limitedSubmissionsSelector,
   isfetched,
   getSubmissionsPage,
+  getSubmissions,
+  getSubmissionById,
 };
