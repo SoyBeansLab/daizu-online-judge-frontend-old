40e6499db4b73c71b965ce7f40337249691b5611
ucpr <contact@ucpr.dev>
2020-05-22 23:20:42 +0900

    feat: submissionsのstateの設計を大きく変更

--- src/state/ducks/submissions/reducers.js
+++ src/state/ducks/submissions/reducers.js
@@ -3,23 +3,56 @@ import types from "./types";
 
 const initState = {
   data: {
-    entities: {},
-    result: {},
+    submissions: {}, // submission一覧を持つための
+    submission: {}, // submission単体をrequestされたとき
   },
   loading: true,
-  ui: {
-    page: 0,
-  },
+  ui: {},
+  pages: {},
+};
+
+const initStatePage = {
+  total: 100,
+  page: 0,
 };
 
 const submissions = (state = initState, action) => {
   switch (action.type) {
     case types.FETCH_SUBMISSIONS:
       return { ...state, loading: true };
-    case types.FETCH_SUBMISSIONS_SUCEESS:
-      return { ...state, loading: false, data: action.data };
-    case types.CHANGE_SUBMISSIONS_PAGE:
-      return { ...state, ui: { page: action.data } };
+    case types.FETCH_SUBMISSIONS_SUCEESS: {
+      const contestId = action.data.contest_id;
+      const page = action.data.page;
+      const total = action.data.total;
+
+      const submissions = state.data.submissions;
+      const pages = state.pages;
+
+      if (!Object.prototype.hasOwnProperty.call(submissions, contestId)) {
+        submissions[contestId] = {};
+        submissions[contestId][page] = {};
+
+        pages[contestId] = initStatePage;
+      }
+
+      submissions[contestId][page] = [...action.data.submissions];
+
+      pages[contestId]["total"] = total;
+
+      return { ...state, loading: false, data: { submissions }, pages };
+    }
+    case types.CHANGE_SUBMISSIONS_PAGE: {
+      const pages = state.pages;
+      const contestId = action.contestId;
+
+      if (!Object.prototype.hasOwnProperty.call(pages, contestId)) {
+        pages[contestId] = {};
+      }
+      pages[contestId]["page"] = action.page;
+      console.log(action);
+
+      return { ...state, pages };
+    }
     default:
       return state;
   }
