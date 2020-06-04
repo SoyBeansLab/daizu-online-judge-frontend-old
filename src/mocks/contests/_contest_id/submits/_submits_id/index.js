339d77e703840c58ec50a8a1fc9bab741fd620e2
ucpr <contact@ucpr.dev>
2020-06-05 01:46:40 +0900

    test: テストデータを修正

--- src/mocks/contests/_contest_id/submits/_submits_id/index.js
+++ src/mocks/contests/_contest_id/submits/_submits_id/index.js
@@ -7,17 +7,19 @@ const f = (contest_id, submit_id) => {
     problem_id: "hogehoge",
     language: "C++",
     score: 100,
-    code_length: 100,
+    code_size: 100,
     result: "AC",
     username: "nemu_sou",
     execute_time: 300,
     memory: 400,
+    all_test_case: 10,
+    passed_test_case: 10,
     source_code: 'printf("HelloWorld");',
   };
 };
 
 module.exports = {
   get({ values }) {
-    return [200, f(values.contest_id, values.submit_id)];
+    return [200, f(values.contest_id, values.submits_id)];
   },
 };
