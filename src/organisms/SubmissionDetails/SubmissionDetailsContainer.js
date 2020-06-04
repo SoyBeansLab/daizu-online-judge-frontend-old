a39ba651d357f581f4ea748f55f03eb5dd72acd7
ucpr <contact@ucpr.dev>
2020-06-05 01:48:13 +0900

    feat: componentが表示されるときにデータをfetchさせるようにした

--- src/organisms/SubmissionDetails/SubmissionDetailsContainer.js
+++ src/organisms/SubmissionDetails/SubmissionDetailsContainer.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import SubmitStatusDetail from "../../molecules/SubmitStatusDetailTable";
 
-import { submissionsSelectors } from "../../state/ducks/submissions";
+import { submissionsOperations, submissionsSelectors } from "../../state/ducks/submissions";
 
 function SubmissionDetailsContainer(props) {
+  const { contestId, submitId } = useParams();
+  const endpoint = `/contests/${contestId}/submits/${submitId}`;
+
+  const fetchSubmission = props.fetchSubmission;
   const submission = props.submission;
 
-  const submitDate = submission.submit_data;
-  const problemName = submission.problem_name;
-  const problemId = submission.problem_id;
+  const submitDate = submission.submitData;
+  const problemName = submission.problemName;
+  const problemId = submission.problemId;
   const username = submission.username;
   const language = submission.language;
   const score = submission.score;
   const result = submission.result;
-  const executeTime = submission.execute_time;
+  const executeTime = submission.executeTime;
   const memory = submission.memory;
-  const testCase = submission.test_case;
+  const testCase = submission.testCase;
+
+  useEffect(() => {
+    fetchSubmission(endpoint);
+  }, [fetchSubmission, endpoint]);
 
   return (
     <SubmitStatusDetail
@@ -38,10 +47,15 @@ function SubmissionDetailsContainer(props) {
 
 SubmissionDetailsContainer.propTypes = {
   submission: PropTypes.object,
+  fetchSubmission: PropTypes.func,
 };
 
 const mapStateToProps = (state, props) => ({
-  submission: submissionsSelectors.getSubmissionById(state, props),
+  submission: submissionsSelectors.getSubmissionsBySubmitId(state, props),
 });
 
-export default connect(mapStateToProps)(SubmissionDetailsContainer);
+const mapDispatchToProps = {
+  fetchSubmission: submissionsOperations.fetchSubmission,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SubmissionDetailsContainer);
