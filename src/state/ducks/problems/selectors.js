c51830dbd6f14637759e6fe53f0bb744a27de25e
ucpr <contact@ucpr.dev>
2020-06-10 00:23:59 +0900

    feat: 問題一覧でデータをfetch済みのときはfetchさせないようにした

--- src/state/ducks/problems/selectors.js
+++ src/state/ducks/problems/selectors.js
@@ -2,7 +2,7 @@ import { createSelector } from "reselect";
 
 const problemsSelector = (state, props) => state.problemsState.problems.data[props.contestId] || {};
 
-const isFetchedProblems = createSelector(problemsSelector, problems => problems === {});
+const isFetchedProblems = createSelector(problemsSelector, problems => Object.keys(problems).length !== 0);
 
 const getProblemsById = createSelector(problemsSelector, problems => Object.values(problems || {}));
 
