a9f8aac5b96a46d0f313e366501375c446aa5d2e
ucpr <contact@ucpr.dev>
2020-06-10 02:47:22 +0900

    feat: コンテスト参加登録のmockデータを追加

---
+++ src/mocks/contests/_contest_id/registrations/index.js
@@ -0,0 +1,26 @@
+const getRegistration = contestId => {
+  const result = {
+    contest_id: contestId,
+    is_registed: true,
+  };
+  return result;
+};
+
+const postRegistration = (contestId, header, params) => {
+  console.log(contestId, header, params);
+  const result = {
+    contestId: contestId,
+    is_registed: true,
+  };
+  return result;
+};
+
+module.exports = {
+  get({ values }) {
+    return [200, getRegistration(values.contest_id)];
+  },
+
+  post({ values, header, params }) {
+    return [200, postRegistration(values.contest_id, header, params)];
+  },
+};
