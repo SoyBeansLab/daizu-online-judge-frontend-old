1f785b3c9acba6607bc3f3dc849e8e4f67829039
ucpr <contact@ucpr.dev>
2020-06-11 00:58:27 +0900

    feat: reselectで更新されなくなったので一旦使わずに実装

--- src/state/ducks/registrations/selectors.js
+++ src/state/ducks/registrations/selectors.js
@@ -1,12 +1,16 @@
-import { createSelector } from "reselect";
+//import { createSelector } from "reselect";
 
 // eslint-disable-next-line
 const registrationsSelector = (state, props) => state.registrationsState.registrations.data || {};
 
+/*
 const isRegisted = createSelector(
   [registrationsSelector, (_, props) => props.contestId],
-  (registrations, contestId) => registrations[contestId]
+  (registrations, contestId) => registrations
 );
+*/
+
+const isRegisted = (state, props) => state.registrationsState.registrations.data[props.contestId] || false;
 
 export default {
   isRegisted,
