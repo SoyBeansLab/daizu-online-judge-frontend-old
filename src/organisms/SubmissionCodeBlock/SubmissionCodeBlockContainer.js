890c186b7d48c9b3010736e47805592bc2cf78d3
ucpr <contact@ucpr.dev>
2020-06-11 01:53:35 +0900

    feat: CodeBlockをContainerとComponentに分けた

--- src/organisms/SubmissionCodeBlock/SubmissionCodeBlockContainer.js
+++ src/organisms/SubmissionCodeBlock/SubmissionCodeBlockContainer.js
@@ -1,29 +1,11 @@
-import React from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
-import CodeBlock from "../../atoms/CodeBlock";
+import SubmissionCodeBlockComponent from "./SubmissionCodeBlockComponent";
 
 import { submissionsSelectors } from "../../state/ducks/submissions";
-
-function SubmissionCodeBlockContainer(props) {
-  console.log(props);
-  const submission = props.submission;
-  const sourceCode = submission.sourceCode || "";
-  const language = submission.language || "";
-
-  return <CodeBlock language={language}>{sourceCode}</CodeBlock>;
-}
-
-SubmissionCodeBlockContainer.propTypes = {
-  submission: PropTypes.shape({
-    sourceCode: PropTypes.string,
-    language: PropTypes.string,
-  }),
-};
 
 const mapStateToProps = (state, props) => ({
   submission: submissionsSelectors.getSubmissionsBySubmitId(state, props),
 });
 
-export default connect(mapStateToProps)(SubmissionCodeBlockContainer);
+export default connect(mapStateToProps)(SubmissionCodeBlockComponent);
