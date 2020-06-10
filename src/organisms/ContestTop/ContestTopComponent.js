c6b5f0c7d29b256651ce621a61b50fadd140b211
ucpr <contact@ucpr.dev>
2020-06-11 01:40:14 +0900

    feat: ContestページのTopコンポーネントをContainerとComponentで別ファイルに分けた

---
+++ src/organisms/ContestTop/ContestTopComponent.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
+
+import TopContents from "../../molecules/ContestTopContents";
+
+function ContestTopComponent(props) {
+  const { contestId } = useParams();
+  const endpoint = `/contests/${contestId}`;
+
+  const content = props.contest.top_content;
+  const fetchContest = props.fetchContest;
+  const isfetchedContest = props.isfetchedContest;
+
+  useEffect(() => {
+    if (!isfetchedContest) {
+      fetchContest(endpoint);
+    }
+  }, [fetchContest, endpoint, isfetchedContest]);
+
+  return <TopContents contestTopContent={content} />;
+}
+
+ContestTopComponent.propTypes = {
+  contest: PropTypes.shape({
+    top_content: PropTypes.string,
+  }),
+  fetchContest: PropTypes.func,
+  isfetchedContest: PropTypes.bool,
+};
+
+export default ContestTopComponent;
