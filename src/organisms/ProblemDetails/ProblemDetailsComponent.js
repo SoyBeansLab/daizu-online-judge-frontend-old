5934cdffd8630e5342454738e453a023d394d03d
ucpr <contact@ucpr.dev>
2020-06-11 01:43:02 +0900

    feat: Problem詳細ページをComponentとContainerにファイルを分けた

---
+++ src/organisms/ProblemDetails/ProblemDetailsComponent.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
+
+import ProblemDetails from "../../molecules/ProblemInfo";
+
+function ProblemDetailsComponent(props) {
+  const { problemId, contestId } = useParams();
+  const url = `/contests/${contestId}/problems/${problemId}`;
+
+  const fetchProblem = props.fetchProblem;
+  const problem = props.problemDetails;
+  const isFetchedProblem = props.isFetchedProblem;
+
+  useEffect(() => {
+    if (!isFetchedProblem) {
+      fetchProblem(url);
+    }
+  }, [isFetchedProblem, fetchProblem, url]);
+
+  return (
+    <ProblemDetails
+      problemId={problemId}
+      problemName={problem.problem_name}
+      problemText={problem.problem_detail}
+      timeLimit={problem.time_limit}
+      memoryLimit={problem.memoryLimit}
+      score={problem.score}
+    />
+  );
+}
+
+ProblemDetailsComponent.propTypes = {
+  fetchProblem: PropTypes.func,
+  problemDetails: PropTypes.object,
+  isFetchedProblem: PropTypes.bool,
+};
+
+export default ProblemDetailsComponent;
--- src/organisms/ProblemDetails/ProblemDetailsContainer.js
+++ src/organisms/ProblemDetails/ProblemDetailsContainer.js
@@ -1,43 +1,8 @@
-import React, { useEffect } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { useParams } from "react-router-dom";
 
-import ProblemDetails from "../../molecules/ProblemInfo";
+import ProblemDetailsComponent from "./ProblemDetailsComponent";
 
 import { problemsOperations, problemsSelectors } from "../../state/ducks/problems";
-
-function ProblemDetailsContainer(props) {
-  const { problemId, contestId } = useParams();
-  const url = `/contests/${contestId}/problems/${problemId}`;
-
-  const fetchProblem = props.fetchProblem;
-  const problem = props.problemDetails;
-  const isFetchedProblem = props.isFetchedProblem;
-
-  useEffect(() => {
-    if (!isFetchedProblem) {
-      fetchProblem(url);
-    }
-  }, [isFetchedProblem, fetchProblem, url]);
-
-  return (
-    <ProblemDetails
-      problemId={problemId}
-      problemName={problem.problem_name}
-      problemText={problem.problem_detail}
-      timeLimit={problem.time_limit}
-      memoryLimit={problem.memoryLimit}
-      score={problem.score}
-    />
-  );
-}
-
-ProblemDetailsContainer.propTypes = {
-  fetchProblem: PropTypes.func,
-  problemDetails: PropTypes.object,
-  isFetchedProblem: PropTypes.bool,
-};
 
 const mapStateToProps = (state, props) => ({
   problemDetails: problemsSelectors.getProblemById(state, props),
@@ -48,4 +13,4 @@ const mapDispatchToProps = {
   fetchProblem: problemsOperations.fetchProblemById,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(ProblemDetailsContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(ProblemDetailsComponent);
