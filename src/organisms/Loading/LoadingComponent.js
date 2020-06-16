742e13c86e36619ccf6793b886700a8567d9785a
ucpr <contact@ucpr.dev>
2020-06-16 23:02:52 +0900

    feat: LoadingのコンポーネントのContainerとComponentを作った

--- src/organisms/Loading/LoadingComponent.js
+++ src/organisms/Loading/LoadingComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import LoadingAtoms from "../atoms/Loading";
+import LoadingAtoms from "../../atoms/Loading";
 
 export default function Loading() {
   return (
---
+++ src/organisms/Loading/LoadingContainer.js
@@ -0,0 +1,9 @@
+import { connect } from "react-redux";
+
+import LoadingComponent from "./LoadingComponent";
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoadingComponent);
