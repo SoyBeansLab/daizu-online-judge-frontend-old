deb1b2e3263bf05b0cc2832c83028a4a5434acef
ucpr <contact@ucpr.dev>
2020-06-27 21:18:23 +0900

    feat: ログイン前はCodeSubmitを表示しないようにした

--- src/templates/Problem.js
+++ src/templates/Problem.js
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import ProblemDetails from "../organisms/ProblemDetails";
 import CodeSubmit from "../organisms/CodeSubmit";
 
+import { useAuth0 } from "../react-auth0-spa";
+
 const useStyles = makeStyles(theme => ({
   root: {
     marginTop: theme.spacing(10),
@@ -14,11 +16,12 @@ const useStyles = makeStyles(theme => ({
 export default function Problem() {
   const classes = useStyles();
   const { contestId, problemId } = useParams();
+  const { isAuthenticated } = useAuth0();
 
   return (
     <div className={classes.root}>
       <ProblemDetails contestId={contestId} problemId={problemId} />
-      <CodeSubmit />
+      {isAuthenticated && <CodeSubmit />}
     </div>
   );
 }
