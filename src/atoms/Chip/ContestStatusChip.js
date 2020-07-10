08362ff850bdf7fdc7c217e315e0726b32b54ce1
ucpr <contact@ucpr.dev>
2020-07-10 21:56:49 +0900

    feat: Contest StatusのChipを実装した

---
+++ src/atoms/Chip/ContestStatusChip.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Chip from "@material-ui/core/Chip";
+
+const useStyles = makeStyles(theme => ({
+  current: {
+    marginRight: theme.spacing(2),
+    borderRadius: 0,
+    minWidth: "76px", // "開催中"と"終了"のステータスChipを"開催予定"と同じ幅にする
+    color: "#FFFFFF",
+    backgroundColor: theme.palette.secondary.main,
+  },
+  upcoming: {
+    marginRight: theme.spacing(2),
+    borderRadius: 0,
+    minWidth: "76px", // "開催中"と"終了"のステータスChipを"開催予定"と同じ幅にする
+    color: "#FFFFFF",
+    backgroundColor: theme.palette.primary.dark,
+  },
+  recent: {
+    marginRight: theme.spacing(2),
+    borderRadius: 0,
+    minWidth: "76px", // "開催中"と"終了"のステータスChipを"開催予定"と同じ幅にする
+    color: "#FFFFFF",
+    backgroundColor: theme.palette.primary.contrastText,
+  },
+}));
+
+export default function ContestStatusChip(props) {
+  const classes = useStyles();
+
+  if (props.contestStatus === "current") {
+    return <Chip className={classes.current} label="開催中" />;
+  } else if (props.contestStatus === "upcoming") {
+    return <Chip className={classes.upcoming} label="開催予定" />;
+  } else {
+    return <Chip className={classes.recent} label="終了" />;
+  }
+}
+
+ContestStatusChip.propTypes = {
+  label: PropTypes.string,
+  contestStatus: PropTypes.string,
+};
