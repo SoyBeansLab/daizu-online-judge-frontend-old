f206a022e8f0a68a8d9b7a00f32c0ca24c69df84
ucpr <contact@ucpr.dev>
2020-08-02 03:35:45 +0900

    test: fix typo

--- src/mocks/notifications/index.js
+++ src/mocks/notifications/index.js
@@ -2,27 +2,27 @@ const notifications = [
   {
     notificationId: "hogehoge1",
     create_date: new Date().toISOString().slice(0, 10),
-    descriptioin: "ver1をリリースしました",
+    description: "ver1をリリースしました",
   },
   {
     notificationId: "hogehoge2",
     create_date: new Date().toISOString().slice(0, 10),
-    descriptioin: "バグだバグだ〜〜！",
+    description: "ver1をリリースしました",
   },
   {
     notificationId: "hogehoge3",
     create_date: new Date().toISOString().slice(0, 10),
-    descriptioin: "これはぴえんこえてぱおん",
+    description: "ver1をリリースしました",
   },
   {
     notificationId: "hogehoge4",
     create_date: new Date().toISOString().slice(0, 10),
-    descriptioin: "ぽえーん",
+    description: "ver1をリリースしました",
   },
   {
     notificationId: "hogehoge5",
     create_date: new Date().toISOString().slice(0, 10),
-    descriptioin: "fix",
+    description: "ver1をリリースしました",
   },
 ];
 
